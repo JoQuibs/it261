000487bc81f3598fd1db30a147d670b0c70e0d37
Jomar Quiban <104065461+JoQuibs@users.noreply.github.com>
2022-11-29 00:19:22 +0800

    Add files via upload

---
+++ it261/website/includes/config.php
@@ -0,0 +1,39 @@
+<?php
+// our config file
+ob_start();  // prevents header errors before reading the whole page!
+define('DEBUG', 'TRUE');  // We want to see our errors
+
+include('./includes/credentials.php');
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+function myError($myFile, $myLine, $errorMsg)
+{
+if(defined('DEBUG') && DEBUG)
+{
+ echo 'Error in file: <b> '.$myFile.' </b> on line: <b> '.$myLine.' </b>';
+      echo 'Error message: <b> '.$errorMsg.'</b>';
+      die();
+  }  else {
+      echo ' Houston, we have a problem!';
+      die();
+  }
+    
+    
+}
+
+// on Hostgator, you may have to use a different value for your host
